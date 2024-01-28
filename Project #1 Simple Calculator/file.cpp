5e4362f967208b046e7c109442bd58ecd0d02e20
Abubakr <thefox5000@gmail.com>
2024-01-28 11:21:35 +0300

    Create add method in calculator

---
+++ Project #1 Simple Calculator/file.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator
+{
+
+private:
+    float _CurrentValue, _OperationValue;
+    string _CurrentOperation = "NULL";
+
+public:
+    void Clear () {
+        _CurrentValue = 0;
+        _OperationValue = 0;
+        _CurrentOperation = "NULL";
+    }
+
+    void Add (float Val) {
+        _CurrentOperation = "Adding";
+        _OperationValue = Val;
+        _CurrentValue += Val;
+    }
+
+    void PrintResult () {
+        cout << "\nResult after " << _CurrentOperation << " " << _OperationValue << " is: " << _CurrentValue;
+    }
+
+};
+
+int main()
+{
+
+    Calculator Calculator1;
+
+    Calculator1.Clear();
+    Calculator1.Add(20);
+    Calculator1.PrintResult();
+    Calculator1.Add(50);
+    Calculator1.PrintResult();
+
+
+    return 0;
+}
