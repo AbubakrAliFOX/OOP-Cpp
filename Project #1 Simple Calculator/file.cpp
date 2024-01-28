95ddf1c31f54c25a9bdb7c3f787a238bfd80b6ed
Abubakr <thefox5000@gmail.com>
2024-01-28 11:38:36 +0300

    Add cancel last operation method to calculator

--- Project #1 Simple Calculator/file.cpp
+++ Project #1 Simple Calculator/file.cpp
@@ -6,46 +6,67 @@ class Calculator
 {
 
 private:
-    float _CurrentValue, _OperationValue;
+    float _CurrentValue, _OldValue, _OperationValue;
     string _CurrentOperation = "NULL";
 
+    bool IsZero(float Number)
+    {
+        return Number == 0;
+    }
+
 public:
-    void Clear () {
+    void Clear()
+    {
         _CurrentValue = 0;
         _OperationValue = 0;
         _CurrentOperation = "NULL";
     }
 
-    void Add (float Val) {
+    void Add(float Val)
+    {
         _CurrentOperation = "Adding";
+        _OldValue = _CurrentValue;
         _OperationValue = Val;
         _CurrentValue += Val;
     }
 
-    void Subtract (float Val) {
+    void Subtract(float Val)
+    {
         _CurrentOperation = "Subtracting";
+        _OldValue = _CurrentValue;
         _OperationValue = Val;
         _CurrentValue -= Val;
     }
 
-    void Divide (float Val) {
+    void Divide(float Val)
+    {
+        _OldValue = _CurrentValue;
+        if (IsZero(Val))
+        {
+            Val = 1;
+        }
         _CurrentOperation = "Dividing";
         _OperationValue = Val;
         _CurrentValue /= Val;
     }
 
-    void Multiply (float Val) {
+    void Multiply(float Val)
+    {
         _CurrentOperation = "Multiplying";
+        _OldValue = _CurrentValue;
         _OperationValue = Val;
         _CurrentValue *= Val;
     }
 
-    
-
-    void PrintResult () {
-        cout << "\nResult after " << _CurrentOperation << " " << _OperationValue << " is: " << _CurrentValue;
+    void CancelLastOperation()
+    {
+        _CurrentValue = _OldValue;
     }
 
+    void PrintResult()
+    {
+        cout << "\nResult after " << _CurrentOperation << " " << _OperationValue << " is: " << _CurrentValue;
+    }
 };
 
 int main()
@@ -58,16 +79,9 @@ int main()
     Calculator1.PrintResult();
     Calculator1.Subtract(50);
     Calculator1.PrintResult();
-    Calculator1.Divide(5);
-    Calculator1.PrintResult();
 
-
-    Calculator1.Clear();
-    Calculator1.Add(5);
+    Calculator1.CancelLastOperation();
     Calculator1.PrintResult();
-    Calculator1.Multiply(22);
-    Calculator1.PrintResult();
-
 
     return 0;
 }
